8981f7498f1d00c4004dbfccd12e52e364c8b933
silverscorpio <rajat.newtoneinstein@gmail.com>
2023-06-09 22:50:51 +0200

    user accounts part finished, functionality implemented

--- web_app/web_apps/views.py
+++ web_app/web_apps/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 # Create your views here.
@@ -12,30 +13,37 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics_ordered = Topic.objects.order_by("date_added")
-    context = {"topics": topics_ordered}
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
+    context = {"topics": topics}
     return render(request=request, context=context, template_name="web_apps/topics.html")
 
 
+@login_required
 def topic(request, topic_id):
     topic_chosen = Topic.objects.get(id=topic_id)
+    if topic_chosen.owner != request.user:
+        raise Http404
     entries = topic_chosen.entry_set.order_by("-date_added")
     context = {"topic": topic_chosen, "entries": entries}
     return render(request=request, context=context, template_name="web_apps/topic.html")
 
 
+@login_required
 def new_topic(request):
     if request.method != "POST":
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect("web_apps:topics")
     context = {"form": form}
     return render(request, 'web_apps/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     topic_chosen = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -51,9 +59,12 @@ def new_entry(request, topic_id):
     return render(request, 'web_apps/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     entry_chosen = Entry.objects.get(id=entry_id)
     topic_chosen = entry_chosen.topic
+    if topic_chosen.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         form = EntryForm(instance=entry_chosen)
